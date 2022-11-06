97143a342296ba8ecc4101d431a01ec0469701e7
JMBRillie <jakerillie@gmail.com>
2022-11-06 22:18:36 +0000

    Changed directory name

--- run_at_boot.sh
+++ run_at_boot.sh
@@ -4,6 +4,6 @@ HOME=/home/pi/
 PYTHONPATH=/home/username/.local/lib/python3.9/site-packages
 
 cd /home/pi/Raspi4/
-/usr/bin/python3 /home/pi/Raspi4/enviroment_CSV.py &
-/usr/bin/python3 /home/pi/Raspi4/CSV2JPG_enviroment.py &
-/usr/bin/python3 /home/pi/Raspi4/GraphFromCSV.py &
+/usr/bin/python3 /home/pi/MonitorPi/enviroment_CSV.py &
+/usr/bin/python3 /home/pi/MonitorPi/CSV2JPG_enviroment.py &
+/usr/bin/python3 /home/pi/MonitorPi/GraphFromCSV.py &
